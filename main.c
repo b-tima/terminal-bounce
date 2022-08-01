dde66a36e29fed02394f99990a04ddd6078e057b
b-tima <morgan.a9904@gmail.com>
2022-08-02 00:35:35 +0200

    check if arguments supplied

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 static terminal_handle_t term_handle;
 
 int main(int argc, char** argv){
+	if(argc < 2){
+		exit(EXIT_FAILURE);
+	}
 	terminal_bounce_init(&term_handle, argv[1]);
 
 	terminal_bounce_play(&term_handle);
